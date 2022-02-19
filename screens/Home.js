92c59b487d6e026cfc8795ce229a160ce1c40ff7
danielmorais123 <72794515+danielmorais123@users.noreply.github.com>
2022-02-19 20:45:14 +0000

    commit

---
+++ screens/Home.js
@@ -0,0 +1,16 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import HeaderTabs from '../components/HeaderTabs'
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <HeaderTabs />
+    </SafeAreaView>
+  )
+}
+
+
+
+
+const styles = StyleSheet.create({})
