d29db4c8f1f6024748df74113f9e617e817ddb12
danielmorais123 <72794515+danielmorais123@users.noreply.github.com>
2022-02-20 13:00:45 +0000

    Home page completed

---
+++ components/RestaurantItem.js
@@ -0,0 +1,82 @@
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+export const restaurants = [
+  {
+    name: "Beachside Bar",
+    categories: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 1200,
+    rating: 4.5,
+  },
+  {
+    name: "Benihana",
+    categories: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 340,
+    rating: 3.7,
+  },
+];
+
+const RestaurantItem = (props) => {
+  return (
+    <TouchableOpacity activeOpacity={0.9} style={{ marginBottom: 30 }}>
+      {props.restaurantData.map((restaurant, index) => (
+        <View
+          key={index}
+          style={{ marginTop: 10, padding: 15, backgroundColor: "white" }}
+        >
+          <RestaurantImage image ={restaurant.image_url}/>
+          <RestaurantInfo name={restaurant.name} rating= {restaurant.rating} />
+        </View>
+      ))}
+    </TouchableOpacity>
+  );
+};
+
+const RestaurantImage = (props) => (
+  <>
+    <Image
+      source={{
+        uri: props.image,
+      }}
+      style={{ width: "100%", height: 180 }}
+    />
+    <TouchableOpacity style={{ position: "absolute", right: 20, top: 20 }}>
+      <MaterialCommunityIcons name="heart-outline" size={25} color="#fff" />
+    </TouchableOpacity>
+  </>
+);
+
+const RestaurantInfo = (props) => (
+  <View
+    style={{
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginTop: 10,
+    }}
+  >
+    <View>
+      <Text style={{ fontSize: 15, fontWeight: "bold" }}>
+        {props.name}
+      </Text>
+      <Text style={{ fontSize: 13, color: "gray" }}>30-45 mins</Text>
+    </View>
+    <View
+      style={{
+        backgroundColor: "#eee",
+        height: 30,
+        width: 30,
+        alignItems: "center",
+        borderRadius: 15,
+        justifyContent: "center",
+      }}
+    >
+      <Text>{props.rating}</Text>
+    </View>
+  </View>
+);
+
+export default RestaurantItem;
--- screens/Home.js
+++ screens/Home.js
@@ -1,16 +1,46 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import HeaderTabs from '../components/HeaderTabs'
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
+import Categories from "../components/Categories";
+import RestaurantItem, { restaurants } from "../components/RestaurantItem";
+
+const YELP_API_KEY =
+  "EmS6LlF3jrdQ5v0_svg8ty6n-axpqhWqZ2blgIUahkFXnoK4ujR6pK9aPPNd8AMgV1rP77MIVZ_ew99CBuXDGicD6eOjALOxCtZRDl1pZ3TskyRag_CDwXXG3zISYnYx";
 
 export default function Home() {
+  const [restaurantData, setRestaurantData] = useState(restaurants);
+
+  const getRestaurantsFromYelpApi = () => {
+    const yelpUrl =
+      "https://api.yelp.com/v3/businesses/search?term=restaurant&location=Hollywood";
+
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY} `,
+      },
+    };
+    return fetch(yelpUrl, apiOptions)
+      .then((res) => res.json())
+      .then((json) => setRestaurantData(json.businesses));
+  };
+
+  useEffect(() => {
+    getRestaurantsFromYelpApi();
+  }, []);
+
   return (
-    <SafeAreaView>
-      <HeaderTabs />
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <RestaurantItem restaurantData={restaurantData} />
+      </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
 
-
-
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
